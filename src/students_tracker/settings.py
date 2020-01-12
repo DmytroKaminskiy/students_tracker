9c87c1ddc445d2087fc8cdcdc858ca1a673d59e7
DmytroKaminskiy <dmytro.kaminskyi92@gmail.com>
2020-01-12 17:01:56 +0200

    update

--- src/students_tracker/settings.py
+++ src/students_tracker/settings.py
@@ -146,6 +146,7 @@ LOGGING = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'testtestapp454545@gmail.com'
 EMAIL_HOST_PASSWORD = 'qwerty123456qwerty'
