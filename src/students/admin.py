3109ba368764e99a7ee8335f5cd92e21f6fb7bbd
Dmytro Kaminskyi <dmytrok@completecase.com>
2020-01-24 16:55:32 +0200

    adw

--- src/students/admin.py
+++ src/students/admin.py
@@ -1,24 +1,33 @@
 from django.contrib import admin
 
-from students.models import Student
+from students.models import Student, Group
 
 
 class StudentAdmin(admin.ModelAdmin):
-    readonly_fields = ('email', )
+    readonly_fields = ('email',)
     list_display = ('id', 'first_name',
                     'last_name', 'email', 'group')
-    list_select_related = ('group', )
+    list_select_related = ('group',)
     list_per_page = 10
 
     def get_readonly_fields(self, request, obj=None):
         readonly_fields = super().get_readonly_fields(request, obj)
 
         if request.user.groups.filter(name='manager').exists():
-            return readonly_fields + ('telephone', )
+            return readonly_fields + ('telephone',)
         return readonly_fields
 
     def has_delete_permission(self, request, obj=None):
         return False
 
 
+class StudentInline(admin.TabularInline):
+    model = Student
+
+
+class GroupAdmin(admin.ModelAdmin):
+    inlines = (StudentInline, )
+
+
 admin.site.register(Student, StudentAdmin)
+admin.site.register(Group, GroupAdmin)
